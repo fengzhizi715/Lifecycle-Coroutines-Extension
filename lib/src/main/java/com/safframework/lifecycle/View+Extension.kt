a0ddef01ea5fbb9ff4653f0e391049914f0f74eb
fengzhizi715 <fengzhizi715@126.com>
2019-10-13 13:47:21 +0800

    add View+Extension.kt

---
+++ lib/src/main/java/com/safframework/lifecycle/View+Extension.kt
@@ -0,0 +1,38 @@
+package com.safframework.lifecycle
+
+import android.view.View
+import kotlinx.coroutines.CompletionHandler
+import kotlinx.coroutines.Job
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.lifecycle.`View+Extension`
+ * @author: Tony Shen
+ * @date: 2019-10-10 13:16
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun View.autoDispose(job: Job) {
+    val listener = ViewListener(this, job)
+    this.addOnAttachStateChangeListener(listener)
+}
+
+private class ViewListener(
+        private val view: View,
+        private val job: Job
+) : View.OnAttachStateChangeListener,
+        CompletionHandler {
+    override fun onViewDetachedFromWindow(v: View) {
+        view.removeOnAttachStateChangeListener(this)
+        job.cancel()
+    }
+
+    override fun onViewAttachedToWindow(v: View) {
+        // do nothing
+    }
+
+    override fun invoke(cause: Throwable?) {
+        view.removeOnAttachStateChangeListener(this)
+        job.cancel()
+    }
+}
