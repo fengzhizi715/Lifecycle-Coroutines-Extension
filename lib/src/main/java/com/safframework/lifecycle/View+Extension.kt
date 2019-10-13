f0def096bb9e643809de5abd0fa42d679d01f59f
fengzhizi715 <fengzhizi715@126.com>
2019-10-13 15:32:19 +0800

    update View+Extension.kt

--- lib/src/main/java/com/safframework/lifecycle/View+Extension.kt
+++ lib/src/main/java/com/safframework/lifecycle/View+Extension.kt
@@ -27,6 +27,11 @@ val View.autoDisposeScope: CoroutineScope
         return ViewCoroutineScope(SupervisorJob() + UI + autoDisposeInterceptor())
     }
 
+val View.asyncAutoDisposeScope: CoroutineScope
+    get() {
+        return ViewCoroutineScope(SupervisorJob() + IO + autoDisposeInterceptor())
+    }
+
 private class ViewCoroutineScope(
         override val coroutineContext: CoroutineContext
 ) : CoroutineScope
