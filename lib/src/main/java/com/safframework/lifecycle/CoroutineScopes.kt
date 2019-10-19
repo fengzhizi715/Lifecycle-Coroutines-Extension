8592aba6411c5dcd510e103603ec49dfd88e1f7d
fengzhizi715 <fengzhizi715@126.com>
2019-10-19 12:48:59 +0800

    add SafeCoroutineScope

--- lib/src/main/java/com/safframework/lifecycle/CoroutineScopes.kt
+++ lib/src/main/java/com/safframework/lifecycle/CoroutineScopes.kt
@@ -1,10 +1,8 @@
 package com.safframework.lifecycle
 
 import com.safframework.lifecycle.listener.CoroutineErrorListener
-import com.safframework.lifecycle.exception.UncaughtCoroutineExceptionHandler
-import kotlinx.coroutines.*
-import java.io.Closeable
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 
 /**
  *
@@ -22,12 +20,3 @@ fun uiScope(errorHandler: CoroutineErrorListener?=null): CoroutineScope = SafeCo
 fun defaultScope(errorHandler: CoroutineErrorListener?=null): CoroutineScope = SafeCoroutineScope(Default,errorHandler)
 
 fun customScope(dispatcher: CoroutineDispatcher,errorHandler: CoroutineErrorListener?=null): CoroutineScope = SafeCoroutineScope(dispatcher,errorHandler)
-
-class SafeCoroutineScope(context: CoroutineContext,errorHandler: CoroutineErrorListener?=null) : CoroutineScope, Closeable {
-
-    override val coroutineContext: CoroutineContext = SupervisorJob() + context + UncaughtCoroutineExceptionHandler(errorHandler)
-
-    override fun close() {
-        coroutineContext.cancelChildren()
-    }
-}
